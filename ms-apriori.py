16e70f275393ae918f6fcef76d59bd85d5e36e68
Amlaan Bhoi <amlaanb@gmail.com>
2018-02-03 14:04:34 -0600

    Added files and read_input function

--- ms-apriori.py
+++ ms-apriori.py
@@ -1,6 +1,16 @@
 import sys
+import csv
 
-def wtf():
-	print("Hello")
-
-wtf()
+def read_input(file_location):
+	input_file = open(file_location, "r")
+	for mindex, m in enumerate(input_file):
+		m = m.replace('{', '')
+		m = m.replace('}', '')
+		m = m.replace('\n', '')
+		temp_set = m.split(', ')
+		temp_set = set(temp_set)
+		print(temp_set)
+if len(sys.argv) == 2:
+	read_input(str(sys.argv[1]))
+else:
+	print("Please run as python ms-apriori.py [input_file].txt")

78479754e229f37a58b7e7e6429b1d5e6c8626ff
Amlaan Bhoi <amlaanb@gmail.com>
2018-02-04 13:38:36 -0600

    Added re, test-data

--- ms-apriori/ms-apriori.py
+++ ms-apriori/ms-apriori.py
@@ -1,14 +1,19 @@
 import sys
 import csv
+import re
 
 # List of values and their respective MIS values
-list_of_mis = []
+mis_dict = {}
 # List of given transactions
 list_of_transactions = []
+# List of cannot-be_together sets
+list_of_cbt = []
 # List of items
 list_of_items = []
+# List of must-have items
+list_of_mh = []
 # Support difference constraint (phi)
-SDC = 0.0
+sdc = 0.0
 
 # Read and parse input file and store each transaction to list_of_transactions
 def read_input(input_location):
@@ -33,42 +38,29 @@ def read_parameter(parameter_location):
 	parameter_file = open(parameter_location, "r")
 	for index, i in enumerate(parameter_file):
 		# Parse MIS to list_of_mis
-		if "MIS" in i:
-			i = i[4:-1]
-			i = i.split(')')
-			item_no = i[0]
-			i = str(i)
-			i = i.split('=')
-			temp_mis = i[1]
-			temp_mis = temp_mis.replace('\']', '')
-			temp_mis = temp_mis.replace(' ', '')
-			list_of_mis.append({int(item_no): float(temp_mis)})
-		# Parse and store SDC
-		elif "SDC" in i:
-			i = i.split(' = ')
-			i = i[1]
-			SDC = i
-		# Custom arguments
-		elif "cannot_be_together" in i:
-			# PARSE THIS and STORE
-			i = i.split(': ')
-			i = str(i)
-			i = i.split(', ')
-			i = i[1:]
-		elif "must-have" in i:
-			i = i.split(': ')
-			i = i[1]
-			i = str(i)
-			i = i.replace('\n', '')
-			i = i.split(' or ')
+		if 'MIS' in i:
+			s = re.findall(r'(\d+)', i)
+			item_no = int(s[0])
+			mis_value = float(s[1] + '.' + s[2])
+			print(str(item_no) + ': ' + str(mis_value))
+			mis_dict.update({item_no: mis_value})
+		elif 'SDC' in i:
+			s = re.findall(r'\d+\.\d+', i)
+			sdc = float(s[0])
+		elif 'cannot_be_together' in i:
+			s = re.findall(r'{\d+[, \d+]*}', i)
+			for e in s:
+				e = re.findall(r'\d+', e)
+				e = [int(x) for x in e]
+				list_of_cbt.append(e)
+		elif 'must-have' in i:
+			s = re.findall(r'\d+', i)
+			s = [int(x) for x in s]
+			list_of_mh = s
 
 # Check for command line arguments
 if len(sys.argv) == 3:
 	read_parameter(str(sys.argv[2]))
 	read_input(str(sys.argv[1]))
-	
-	print(list_of_items)
-	print(list_of_mis)
-	print(list_of_transactions)
 else:
 	print("Please run as python ms-apriori.py [input_file].txt [parameter_file].txt")
